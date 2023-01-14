87fda32491f4adc79b585b2731910c86e191c9dc
salah <salaht321@gmail.com>
2023-01-15 00:17:31 +0200

    update

--- src/main/java/com/example/dijkstra/Test.java
+++ src/main/java/com/example/dijkstra/Test.java
@@ -9,7 +9,7 @@ import java.util.Set;
 
 public class Test {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner scan = new Scanner(new File("C:\\Users\\hp\\IdeaProjects\\Dijkstra\\src\\main\\resources\\com\\example\\dijkstra\\test.txt"));
+        Scanner scan = new Scanner(new File("test.txt"));
         HashMap<Country, LinkedList<Node>> graph = new HashMap<>();
 
         double countries = scan.nextDouble();
@@ -22,8 +22,8 @@ public class Test {
 
         while (edges-- != 0) {
             String[] tokens = scan.nextLine().split(" ");
-            Country c1 = get(new Country(tokens[0], 0, 0), graph.keySet());
-            Country c2 = get(new Country(tokens[1], 0, 0), graph.keySet());
+            Country c1 = get(new Country(tokens[0]), graph.keySet());
+            Country c2 = get(new Country(tokens[1]), graph.keySet());
 
             if (c1 == null || c2 == null) {
                 System.out.println("you have error in edges");
@@ -34,6 +34,47 @@ public class Test {
         }
 
         graph.forEach((country, nodes) -> System.out.println(country + " " + nodes));
+
+        dijkstra(graph, get(new Country("Country3"), graph.keySet()), get(new Country("Country5"), graph.keySet()));
+    }
+
+    public static void dijkstra(HashMap<Country, LinkedList<Node>> graph, Country from, Country to) {
+        from.setDistance(0);
+
+        Country v, w;
+
+        while (true) {
+            v = smallest(graph);
+            if (v == null)
+                return;
+            get(v, graph.keySet()).setKnown(true);
+
+            LinkedList<Node> list = graph.get(v);
+            for (int i = 0; i < list.size(); i++) {
+                if (!list.get(i).country().getKnown())
+                    if (list.get(i).country().getDistance() + list.get(i).cost() < list.get(i).country().getDistance()) {
+                        list.get(i).country().setDistance(from.getDistance() + list.get(i).cost());
+                        list.get(i).country().setPath(v);
+                        System.out.println(v);
+                    }
+            }
+
+        }
+
+
+    }
+
+    static Country country = null;
+    static int min = Integer.MAX_VALUE;
+
+    private static Country smallest(HashMap<Country, LinkedList<Node>> graph) {
+        graph.forEach((c, l) -> {
+            if (l.size() < min) {
+                min = l.size();
+                country = c;
+            }
+        });
+        return country;
     }
 
     private static double findCost(Country c1, Country c2) {
