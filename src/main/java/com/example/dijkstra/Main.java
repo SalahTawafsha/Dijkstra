c52c87f3ca4f4d06fff4c819cecb409550a7e5d5
salah tawafsha <1200339@student.birzeit.edu>
2023-01-28 19:34:21 +0200

    update

--- src/main/java/com/example/dijkstra/Main.java
+++ src/main/java/com/example/dijkstra/Main.java
@@ -3,16 +3,19 @@ package com.example.dijkstra;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("map.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setMaximized(true);
+        stage.getIcons().add(new Image("icon.png"));
+        stage.setTitle("Map Application");
         stage.setScene(scene);
 
         stage.show();
